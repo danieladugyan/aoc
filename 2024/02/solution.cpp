bbcd884c956ce7c3c00e7cf68cc8b2da6a63ac4f
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-02 08:01:35 +0100

    2024: Day 2.2

--- 2024/02/solution.cpp
+++ 2024/02/solution.cpp
@@ -2,50 +2,94 @@
 
 using namespace std;
 
-bool comp(int a, int b) { return a > b; }
+bool check(int curr, int next) { return next - curr >= 1 && next - curr <= 3; }
 
 int main() {
     string s;
-    vector<int> level;
+    vector<int> levels;
     int a;
-    int sorted = 0;
-    bool fail = false;
+    int reports = 0;
+    int failed = 0;
+
+    // read every line (report)
     while (getline(cin, s)) {
         stringstream ss(s);
+        // push every number into a vector
         while (ss >> a) {
-            level.push_back(a);
+            levels.push_back(a);
         }
 
+        // is the array increasing or decreasing?
+        if (levels.front() > levels.back()) {
+            reverse(levels.begin(), levels.end());
+        }
+
+        cout << "diff: " << levels.back() - levels.front() << endl;
         cout << "level: ";
-        for (auto &&i : level) {
+        for (auto &&i : levels) {
             cout << i << ", ";
         }
         cout << endl;
 
-        if (level[0] > level[1]) {
-            reverse(level.begin(), level.end());
-        }
-        for (int i = 0; i < level.size() - 1; i++) {
-            cout << level[i] << " " << level[i + 1];
-            if (!(level[i + 1] - level[i] >= 1 &&
-                  level[i + 1] - level[i] <= 3)) {
-                fail = true;
-                cout << " fail" << endl;
-                break;
+        unsigned int i = 0;
+        while (i < levels.size() - 1) {
+            int curr = levels[i], next = levels[i + 1],
+                nextnext = levels[i + 2];
+            cout << curr << " " << next << " ";
+
+            if (check(curr, next)) {
+                cout << "pass" << endl;
             } else {
-                cout << " pass" << endl;
+                if (failed == 0) {
+                    if (i + 2 >= levels.size()) {
+                        cout << "first fail at end of report -> pass!";
+                        break;
+                    }
+
+                    failed++;
+                    if (i > 0) { // <-- forgot the edge case where i = 0
+                        if (check(levels[i - 1], next)) {
+                            cout << "saved, delete " << curr << endl;
+                            levels.erase(levels.begin() + i);
+                            // i--;
+                            continue;
+                        }
+                    } else {
+                        if (check(next, nextnext)) {
+                            cout << "saved, delete " << curr << endl;
+                            levels.erase(levels.begin() + i);
+                            // i--;
+                            continue;
+                        }
+                    }
+
+                    if (check(curr, nextnext)) {
+                        cout << "saved, delete " << next << endl;
+                        levels.erase(levels.begin() + i + 1);
+                        i++;
+                        continue;
+                    }
+
+                    cout << "lookahead failed" << endl;
+                    failed++;
+                    break;
+                } else {
+                    cout << "fail, again." << endl;
+                    failed++;
+                    break;
+                }
             }
+
+            i++;
         }
 
-        if (!fail) {
-            sorted++;
-        } else {
-            fail = false;
+        if (failed < 2) {
+            reports++;
         }
+        failed = 0;
+        levels.clear();
 
-        cout << sorted << endl;
-
-        level.clear();
+        cout << reports << endl;
     }
-    cout << sorted << endl;
+    cout << reports << endl;
 }
