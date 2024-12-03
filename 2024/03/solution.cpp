351bdf0e555401bd691bb8cab97fac9f084ff6bc
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-03 07:11:14 +0100

    2024: Day 3.2

--- 2024/03/solution.cpp
+++ 2024/03/solution.cpp
@@ -6,7 +6,44 @@ int main() {
     char c;
     int a, b;
     int sum = 0;
+    bool enabled = true;
+    
+    // Sometimes in life, one must ask of themselves
+    // not whether they can, but whether they should...
+
     while (cin >> noskipws >> c) {
+        if (c == 'd') {
+            cin >> noskipws >> c;
+            if (c == 'o') {
+                cin >> noskipws >> c;
+                if (c == '(') {
+                    cin >> noskipws >> c;
+                    if (c == ')') {
+                        cin >> noskipws >> c;
+                        enabled = true;
+                    }
+                } else if (c == 'n') {
+                    cin >> noskipws >> c;
+                    if (c == '\'') {
+                        cin >> noskipws >> c;
+                        if (c == 't') {
+                            cin >> noskipws >> c;
+                            if (c == '(') {
+                                cin >> noskipws >> c;
+                                if (c == ')') {
+                                    enabled = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!enabled) {
+            continue;
+        }
+
         if (c == 'm') {
             cin >> noskipws >> c;
             if (c == 'u') {
