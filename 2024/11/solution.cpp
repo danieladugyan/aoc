39e36dcd1ee544e2b159aed68f4bb54986920cc9
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-11 11:23:13 +0100

    2024: Day 11.2

--- 2024/11/solution.cpp
+++ 2024/11/solution.cpp
@@ -3,6 +3,37 @@
 using namespace std;
 #define N 75
 
+map<pair<long, long>, long> cache;
+
+long get_stones(long stone, long n) {
+    if (n == 0) return 1;
+
+    pair<long, long> p = make_pair(stone, n);
+    if (cache.count(p)) {
+        // cout << "cache hit: " << p.first << ", " << p.second << ": " << cache[p] << endl;
+        return cache[p];
+    }
+
+    long digits = 0;
+    string s = to_string(stone);
+    digits = s.length();
+
+    long res = 0;
+    if (stone == 0) {
+        res = get_stones(1, n - 1);
+    } else if (digits % 2 == 0) {
+        string s1 = s.substr(0, digits / 2);
+        string s2 = s.substr(digits / 2, digits);
+        res += get_stones(stol(s1), n - 1);
+        res += get_stones(stol(s2), n - 1);
+    } else {
+        res = get_stones(stone * 2024, n - 1);
+    }
+    cache[p] = res;
+
+    return res;
+}
+
 int main() {
     vector<long> stones;
     long stone;
@@ -10,32 +41,12 @@ int main() {
         stones.push_back(stone);
     }
 
-    string s, s1, s2;
-    int digits;
-    for (int n = 0; n < N; n++) {
-        for (int i = 0; i < stones.size(); i++) {
-            stone = stones[i];
-            s = to_string(stone);
-            digits = s.length();
-
-            if (stone == 0) {
-                stones[i] = 1;
-            } else if (digits % 2 == 0) {
-                s1 = s.substr(0, digits / 2);
-                s2 = s.substr(digits / 2, digits);
-                stones[i] = stol(s1);
-                stones.insert(stones.begin() + i + 1, stoi(s2));
-                i++;
-            } else {
-                stones[i] *= 2024;
-            }
-        }
-
-        // for (auto &&i : stones) {
-        //     cout << i << ", ";
-        // }
-        // cout << endl;
-        // cout << "--------------------" << endl;
+    long length = 0;
+    for (auto &&stone : stones) {
+        long res = get_stones(stone, N);
+        // cout << stone << ": " << res << endl;
+        length += res;
     }
-    cout << stones.size() << endl;
+
+    cout << length << endl;
 }
