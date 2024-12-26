77a0a39cb325436c5467e15649a9f613b1a09bc8
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-26 20:05:50 +0100

    2024: Day 18.2

--- 2024/18/solution.cpp
+++ 2024/18/solution.cpp
@@ -1,52 +1,70 @@
 #include <bits/stdc++.h>
-#define RANGE 70   // 70
+#define RANGE 70    // 70
 #define BYTES 1024  // 1024
 using namespace std;
 
 typedef pair<int, int> vec2;
 
+template <typename T, typename Y>
+auto operator<<(ostream &stream, pair<T, Y> const &p) -> ostream & {
+    stream << "(" << p.first << ',' << p.second << ")";
+    return stream;
+}
+
 int main() {
     set<vec2> falling;
+    queue<vec2> more_falling;
     int row, col;
     while (scanf("%d,%d\n", &col, &row) != EOF) {
-        falling.insert({row, col});
-
         if (falling.size() >= BYTES) {
-            break;
+            more_falling.push({row, col});
+        } else {
+            falling.insert({row, col});
         }
     }
 
-    priority_queue<pair<int, vec2>> q;
-    set<vec2> processed;
-    map<vec2, int> distance;
-    for (row = 0; row <= RANGE; row++) {
-        for (col = 0; col <= RANGE; col++) {
-            distance[{row, col}] = INT_MAX;
-        }
-    }
-    distance[{0, 0}] = 0;
-    q.push({0, {0, 0}});
+    while (more_falling.size() > 0) {
+        vec2 next_falling_byte = more_falling.front();
+        falling.insert(next_falling_byte);
+        more_falling.pop();
 
-    while (!q.empty()) {
-        vec2 a = q.top().second;
-        q.pop();
-        if (processed.count(a)) continue;
-        processed.insert(a);
-
-        for (auto dir : vector<vec2>{{0, 1}, {1, 0}, {-1, 0}, {0, -1}}) {
-            vec2 b = {a.first + dir.first, a.second + dir.second};
-            bool in_bounds = 0 <= b.first && b.first <= RANGE && 0 <= b.second &&
-                             b.second <= RANGE;
-            bool is_wall = falling.count(b);
-            if (!in_bounds || is_wall) continue;
-
-            int new_distance = distance[a] + 1;
-            if (new_distance < distance[b]) {
-                distance[b] = new_distance;
-                q.push({-new_distance, b});
+        priority_queue<pair<int, vec2>> q;
+        set<vec2> processed;
+        map<vec2, int> distance;
+        for (row = 0; row <= RANGE; row++) {
+            for (col = 0; col <= RANGE; col++) {
+                distance[{row, col}] = INT_MAX;
             }
         }
-    }
+        distance[{0, 0}] = 0;
+        q.push({0, {0, 0}});
 
-    cout << distance[{RANGE, RANGE}] << endl;
+        while (!q.empty()) {
+            vec2 a = q.top().second;
+            q.pop();
+            if (processed.count(a)) continue;
+            processed.insert(a);
+
+            for (auto dir : vector<vec2>{{0, 1}, {1, 0}, {-1, 0}, {0, -1}}) {
+                vec2 b = {a.first + dir.first, a.second + dir.second};
+                bool in_bounds = 0 <= b.first && b.first <= RANGE &&
+                                 0 <= b.second && b.second <= RANGE;
+                bool is_wall = falling.count(b);
+                if (!in_bounds || is_wall) continue;
+
+                int new_distance = distance[a] + 1;
+                if (new_distance < distance[b]) {
+                    distance[b] = new_distance;
+                    q.push({-new_distance, b});
+                }
+            }
+        }
+
+        int d = distance[{RANGE, RANGE}];
+        if (d == INT_MAX) {
+            cout << next_falling_byte << endl;
+            break;
+        }
+        // cout << next_falling_byte << " " << d << endl;
+    }
 }
