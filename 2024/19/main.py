cea3f605408cb1e4f001985bd049a422def1a16d
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-26 23:30:53 +0100

    2024: Day 19.2

--- 2024/19/main.py
+++ 2024/19/main.py
@@ -1,33 +1,35 @@
 from functools import cache
 from sys import stdin
 
-patterns = list(map(lambda x: x.strip(), input().split(',')))
+patterns = list(map(lambda x: x.strip(), input().split(",")))
 designs = []
 
-input() # skip newline
+input()  # skip newline
 for line in stdin.readlines():
     designs.append(line.strip())
 
 # print("patterns:", patterns)
 # print("designs:", designs)
 
+
 @cache
-def possible(s: str):
+def possible(s: str) -> int:
     matching = list(filter(lambda p: s.startswith(p), patterns))
 
+    ways = 0
     for m in matching:
         if m == s:
-            return True
-        
-        if possible(s[len(m):]):
-            return True
-    
-    return False
+            ways += 1
 
-if __name__ == '__main__':
+        if p := possible(s[len(m) :]):
+            ways += p
+
+    return ways
+
+
+if __name__ == "__main__":
     answer = 0
     for design in designs:
-        if possible(design):
-            answer += 1
-    
+        answer += possible(design)
+
     print(answer)
