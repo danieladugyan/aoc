68a4904e567b9de71797955e99fbf767010621e9
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-26 19:48:44 +0100

    2024: Day 18.1

--- 2024/18/solution.cpp
+++ 2024/18/solution.cpp
@@ -1,39 +1,52 @@
 #include <bits/stdc++.h>
-#define WIDTH 7   // 70
-#define HEIGHT 7  // 70
-#define BYTES 12  // 1024
+#define RANGE 70   // 70
+#define BYTES 1024  // 1024
 using namespace std;
 
 typedef pair<int, int> vec2;
 
-template <typename T, typename Y>
-auto operator<<(ostream& stream, pair<T, Y> const& p) -> ostream& {
-    stream << "(" << p.first << ',' << p.second << ")";
-    return stream;
-}
-
 int main() {
-    vector<vec2> falling;
-    int x, y;
-    char mem[WIDTH][HEIGHT] = {};
-    int i = 0;
-    while (scanf("%d,%d\n", &x, &y) != EOF) {
-        falling.push_back({x, y});
-        mem[x][y] = '#';
-        
-        if (i >= BYTES) {
+    set<vec2> falling;
+    int row, col;
+    while (scanf("%d,%d\n", &col, &row) != EOF) {
+        falling.insert({row, col});
+
+        if (falling.size() >= BYTES) {
             break;
         }
     }
 
-    for (x = 0; x < WIDTH; x++) {
-        for (y = 0; y < WIDTH; y++) {
-            if (mem[x][y] == 0) {
-                cout << ".";
-            } else {
-                cout << mem[x][y];
+    priority_queue<pair<int, vec2>> q;
+    set<vec2> processed;
+    map<vec2, int> distance;
+    for (row = 0; row <= RANGE; row++) {
+        for (col = 0; col <= RANGE; col++) {
+            distance[{row, col}] = INT_MAX;
+        }
+    }
+    distance[{0, 0}] = 0;
+    q.push({0, {0, 0}});
+
+    while (!q.empty()) {
+        vec2 a = q.top().second;
+        q.pop();
+        if (processed.count(a)) continue;
+        processed.insert(a);
+
+        for (auto dir : vector<vec2>{{0, 1}, {1, 0}, {-1, 0}, {0, -1}}) {
+            vec2 b = {a.first + dir.first, a.second + dir.second};
+            bool in_bounds = 0 <= b.first && b.first <= RANGE && 0 <= b.second &&
+                             b.second <= RANGE;
+            bool is_wall = falling.count(b);
+            if (!in_bounds || is_wall) continue;
+
+            int new_distance = distance[a] + 1;
+            if (new_distance < distance[b]) {
+                distance[b] = new_distance;
+                q.push({-new_distance, b});
             }
         }
-        cout << endl;
     }
+
+    cout << distance[{RANGE, RANGE}] << endl;
 }
