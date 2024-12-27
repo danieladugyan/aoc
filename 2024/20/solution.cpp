460fb8f54b362267b1cf4e2b21f3819775bea066
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-27 13:11:24 +0100

    2024: Day 20.2

--- 2024/20/solution.cpp
+++ 2024/20/solution.cpp
@@ -8,7 +8,6 @@ int main() {
         m.push_back(s);
     }
 
-    set<vec2> path;
     priority_queue<pair<int, vec2>> q;
     set<vec2> processed;
     map<vec2, int> distance;
@@ -18,7 +17,6 @@ int main() {
             distance[{row, col}] = INT_MAX;
             if (m[row][col] != '#') {
                 shortest_path += 1;
-                path.insert({row, col});
             }
             if (m[row][col] == 'S') {
                 start = {row, col};
@@ -32,71 +30,63 @@ int main() {
         }
     }
 
-    set<pair<vec2, vec2>> cheats;
-    for (auto &&p : path) {
-        for (auto &&d1 : DIRS) {
-            vec2 cheat1 = p + d1;
-            char c1 = m[cheat1.first][cheat1.second];
-            bool in_wall = cheat1.first == 0 || cheat1.first == m.size() - 1 ||
-                           cheat1.second == 0 ||
-                           cheat1.second == m[cheat1.first].size() - 1;
-            if (c1 != '#' || in_wall) {
-                continue;  // TODO: does c1 really need to be an obstacle?
-            }
-            m[cheat1.first][cheat1.second] = '1';
-            for (auto &&d2 : DIRS) {
-                vec2 cheat2 = cheat1 + d2;
-                char c2 = m[cheat2.first][cheat2.second];
-                bool in_wall = cheat2.first == 0 ||
-                               cheat2.first == m.size() - 1 ||
-                               cheat2.second == 0 ||
-                               cheat2.second == m[cheat2.first].size() - 1;
-                if (c2 == '#' || in_wall || d1 + d2 == vec2{0, 0}) continue;
-                m[cheat2.first][cheat2.second] = '2';
+    map<vec2, vec2> prev;
+    while (!q.empty()) {
+        vec2 curr = q.top().second;
+        q.pop();
 
-                // Dijkstra with "cheat" instance
-                priority_queue<pair<int, vec2>> q_copy = q;
-                set<vec2> processed_copy = processed;
-                map<vec2, int> distance_copy = distance;
-                bool been_at_1 = false;
-                while (!q_copy.empty()) {
-                    vec2 curr = q_copy.top().second;
-                    q_copy.pop();
+        if (processed.count(curr)) continue;
+        processed.insert(curr);
 
-                    if (processed_copy.count(curr)) continue;
-                    processed_copy.insert(curr);
-                    if (m[curr.first][curr.second] == '1') been_at_1 = true;
-
-                    for (auto &&dir : DIRS) {
-                        vec2 next = curr + dir;
-                        char c = m[next.first][next.second];
-                        if (c != '#') {
-                            if (c == '2' && !been_at_1) continue;
-                            int new_distance = distance_copy[curr] + 1;
-                            if (new_distance < distance_copy[next]) {
-                                distance_copy[next] = new_distance;
-                                q_copy.push({-new_distance, next});
-                            }
-                        }
-                    }
+        for (auto&& dir : DIRS) {
+            vec2 next = curr + dir;
+            char c = m[next.first][next.second];
+            if (c != '#') {
+                int new_distance = distance[curr] + 1;
+                if (new_distance < distance[next]) {
+                    distance[next] = new_distance;
+                    prev[next] = curr;
+                    q.push({-new_distance, next});
                 }
-
-                int diff = shortest_path - distance_copy[goal];
-                if (diff >= 100) {
-                    // for (auto &&line : m) {
-                    //     cout << line << endl;
-                    // }
-                    // cout << "diff = " << diff << endl;
-                    // cout << p << "," << cheat2 << endl;
-                    // cout << "----------" << endl;
-                    cheats.insert({p, cheat2});
-                }
-
-                m[cheat2.first][cheat2.second] = c2;
             }
-            m[cheat1.first][cheat1.second] = c1;
         }
     }
-    cout << cheats.size() << endl;
 
+    vector<vec2> path;
+    vec2 curr = goal;
+    while (prev.count(curr)) {
+        path.insert(path.begin(), curr);
+        curr = prev[curr];
+    }
+    path.insert(path.begin(), start);
+
+    int cheats = 0;
+    for (unsigned i = 0; i < path.size(); i++) {
+        for (unsigned j = i; j < path.size(); j++) {
+            auto&& p1 = path[i];
+            auto&& p2 = path[j];
+            vec2 v = p2 - p1;
+            int length = abs(v.first) + abs(v.second);
+            if (length > 20) continue;
+
+            int cheat_distance = 0;
+            for (unsigned pi = 0; pi < path.size(); pi++) {
+                auto&& p = path[pi];
+                if (p == p1) {
+                    cheat_distance += length;
+                    pi = j;
+                } else {
+                    cheat_distance += 1;
+                }
+            }
+
+            int diff = shortest_path - cheat_distance;
+            // cout << "diff = " << diff << endl;
+            if (diff >= 100) {
+                cheats += 1;
+            }
+        }
+    }
+
+    cout << cheats << endl;
 }
