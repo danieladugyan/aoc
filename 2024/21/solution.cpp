7c0b0ba92268e756133a7b35de844b5a3a4bb049
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-27 19:42:20 +0100

    2024: Day 21.1

--- 2024/21/solution.cpp
+++ 2024/21/solution.cpp
@@ -1,13 +1,9 @@
-#include <bits/stdc++.h>
-
-using namespace std;
+#include "lib.hpp"
 
 #define NUMPAD_ROWS 4
 #define NUMPAD_COLS 3
 #define KEYPAD_ROWS 2
 #define KEYPAD_COLS 3
-
-typedef complex<int> vec2;
 
 char numpad[NUMPAD_ROWS][NUMPAD_COLS] = {
     {'7', '8', '9'},
@@ -23,33 +19,44 @@ char keypad[KEYPAD_ROWS][KEYPAD_COLS] = {
 
 map<char, vec2> num_coords;
 map<char, vec2> key_coords;
+map<vec2, char> dir_move = {
+    {{0, 1}, '>'},
+    {{0, -1}, '<'},
+    {{1, 0}, 'v'},
+    {{-1, 0}, '^'},
+};
 
-string walk(vec2* origin, vec2 goal) {
-    string moves;
-    vec2 pos = *origin;
-    for (vec2 delta = goal - pos; delta != 0; delta = goal - pos) {
-        // positive i = down
-        if (delta.imag() > 0) {
-            pos += complex<int>({0, 1});
-            pos.imag(pos.imag() + 1);
-            moves.push_back('v');
-        }
-
-        if (delta.imag() < 0) {
-            pos.imag(pos.imag() - 1);
-            moves.push_back('^');
-        }
-
-        if (delta.real() > 0) {
-            pos.real(pos.real() + 1);
-            moves.push_back('>');
-        }
-
-        if (delta.real() < 0) {
-            pos.real(pos.real() - 1);
-            moves.push_back('<');
-        }
+string walk(vec2 robot_pos, vec2 goal, vec2 danger) {
+    string row_moves;
+    string col_moves;
+    vec2 diff = goal - robot_pos;
+    int d_row = diff.first;
+    if (d_row > 0) {
+        row_moves.append(string(d_row, 'v'));
+    } else if (d_row < 0) {
+        row_moves.append(string(-d_row, '^'));
     }
+
+    int d_col = diff.second;
+    if (d_col > 0) {
+        col_moves.append(string(d_col, '>'));
+    } else if (d_col < 0) {
+        col_moves.append(string(-d_col, '<'));
+    }
+
+    // avoid disallowed corner of pads
+    if (vec2{goal.first, robot_pos.second} == danger) {
+        return col_moves + row_moves + 'A';
+    };
+    if (vec2{robot_pos.first, goal.second} == danger) {
+        return row_moves + col_moves + 'A';
+    };
+
+    // Why is it optimal to go left first (if needed), otherwise up/down?
+    if (d_col < 0) {
+        return col_moves + row_moves + 'A';
+    }
+    return row_moves + col_moves + 'A';
 }
 
 int main() {
@@ -67,16 +74,42 @@ int main() {
         }
     }
 
-    vec2 robot1 = num_coords['A'];
-    vec2 robot2 = key_coords['A'];
-    vec2 robot3 = key_coords['A'];
-
+    int answer = 0;
     string pin;
     while (getline(cin, pin)) {
-        // cout << pin << ":";
+        int pin_numeric = stoi(pin.substr(0, 3));
 
+        vec2 robot1 = num_coords['A'];
+        string moves1;
         for (auto&& c : pin) {
-            auto moves = walk(&robot1, num_coords[c]);
+            vec2 next = num_coords[c];
+            moves1 += walk(robot1, next, num_coords[EOF]);
+            robot1 = next;
         }
+
+        vec2 robot2 = key_coords['A'];
+        string moves2;
+        for (auto&& c : moves1) {
+            vec2 next = key_coords[c];
+            moves2 += walk(robot2, next, key_coords[EOF]);
+            robot2 = next;
+        }
+
+        vec2 robot3 = key_coords['A'];
+        string moves3;
+        for (auto&& c : moves2) {
+            vec2 next = key_coords[c];
+            moves3 += walk(robot3, next, key_coords[EOF]);
+            robot3 = next;
+        }
+
+        // cout << pin << ": " << moves3 << endl;
+        // cout << moves3.size() << " * " << pin_numeric << endl;
+        answer += moves3.size() * pin_numeric;
+        // cout << moves3 << endl;
+        // cout << moves2 << endl;
+        // cout << moves1 << endl;
+        // cout << pin << endl;
     }
+    cout << answer << endl;
 }
