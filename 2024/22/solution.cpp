d96d5e7f327dd79832582a589affaed03a070b8a
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-28 18:10:52 +0100

    2024: Day 22.2

--- 2024/22/solution.cpp
+++ 2024/22/solution.cpp
@@ -28,10 +28,25 @@ int main() {
     // cout << "/32: got " << (450 >> 5) << ", expected 14" << endl;
     // cout << "*2048: got " << (450 >> 5) << ", expected 14" << endl;
 
-    num answer = 0;
+    vector<vector<int>> prices;
+    vector<vector<int>> priceDeltas;
+    int idx = 0;
     while (cin >> secret) {
-        // cout << secret << ": ";
+        int prev = 0, curr = 0;
+        prices.push_back(vector<int>{});
+        priceDeltas.push_back(vector<int>{});
+
         for (int i = 0; i < N; i++) {
+            curr = secret % 10;
+            if (i == 0) {
+                // cout << secret << ": " << curr << endl;
+            } else {
+                prices[idx].push_back(curr);
+                priceDeltas[idx].push_back(curr - prev);
+                // cout << secret << ": " << curr << " (" << curr - prev << ")"
+                //  << endl;
+            }
+
             tmp = secret << 6;
             mix(&secret, tmp);
             prune(&secret);
@@ -43,9 +58,43 @@ int main() {
             tmp = secret << 11;
             mix(&secret, tmp);
             prune(&secret);
+
+            prev = curr;
         }
-        answer += secret;
-        // cout << secret << endl;
+        idx++;
+        // cout << endl << endl;
     }
-    cout << answer << endl;
+
+    // cout << prices << endl;
+    // cout << priceDeltas << endl;
+
+    int best_price = 0;
+    for (int a = -9; a <= 9; a++) {
+        cout << a << endl;
+        for (int b = -9; b <= 9; b++) {
+            cout << "\t" << b << endl;
+            for (int c = -9; c <= 9; c++) {
+                for (int d = -9; d <= 9; d++) {
+                    vector<int> changes{a, b, c, d};
+
+                    int total = 0;
+                    for (unsigned i = 0; i < priceDeltas.size(); i++) {
+                        auto xs = priceDeltas[i];
+                        auto it = search(xs.begin(), xs.end(), changes.begin(),
+                                         changes.end());
+                        if (it != xs.end()) {
+                            int idx = distance(xs.begin(), it);
+                            total += prices[i][idx + 3];
+                        }
+                    }
+                    if (total > best_price) {
+                        // cout << total << ": " << changes << endl;
+                        best_price = total;
+                    }
+                }
+            }
+        }
+    }
+    // cout << secret << endl;
+    cout << best_price << endl;
 }
