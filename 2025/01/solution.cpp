6eeb0b95f0a045b3a593e9f47e33c562008f3578
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2025-12-01 10:16:34 +0100

    2025: Day 1.2

--- 2025/01/solution.cpp
+++ 2025/01/solution.cpp
@@ -1,36 +1,70 @@
 #include <bits/stdc++.h>
+#include <cassert>
+#include <cstdlib>
 
 using namespace std;
 
+int mod(int x, int div) {
+  int res = x % div;
+  if (res < 0) {
+    res += div;
+  }
+  return res;
+}
+
 int main() {
   int ans = 0;
+
   int x = 50;
-  
+  int diff = 0;
+
   char c;
   int a;
   string s;
+
   while (getline(cin, s)) {
     stringstream ss(s);
     ss >> c >> a;
-    
+
     if (c == 'L') {
-      x -= a;
+      diff = -a;
     } else {
-      x += a;
+      diff = a;
     }
-    x %= 100;
 
-    if (x == 0) ans++;
-  }
-
-  // for (int &i : xs) {
-  //   cout << i << endl;
-  // }
-
-  // x = 0;
-  // for (unsigned i = 0; i < xs.size(); i++) {
+    cout << x << " rotated by " << diff << " | " << ans << endl;
     
-  // }
+    if (0 < (x + diff) && (x + diff) < 100) {
+      x += diff;
+    } else {
+      int non_zero = x != 0;
+      int remaining_diff;
+      if (x == 0 || diff < 0) {
+        remaining_diff = diff + x;
+      } else {
+        remaining_diff = diff - (100 - x);
+      }
+      int remaining_turns = abs(remaining_diff) / 100;
+      int last_rotate = mod(remaining_diff, 100);
+      
+      // Unless we started at zero, we'll at least go past once.
+      if (non_zero) ans += 1;
+
+      // And then me may do some more full turns.
+      ans += remaining_turns;
+
+      // Then the dial reaches its final point.
+      assert(last_rotate == mod(x + diff, 100));
+      x = last_rotate;
+
+      cout
+        << "\t diff = " << diff << endl
+        << "\t started at non-zero? " << non_zero << endl
+        << "\t remaining diff = " << remaining_diff << endl
+        << "\t full turns left = " << remaining_turns << endl
+        << "\t last rotate = " << last_rotate << endl;
+    }
+  }
 
   cout << "answer = " << ans << endl;
 }
