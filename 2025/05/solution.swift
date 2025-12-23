dae86e53c967879a9d03816ef3f879c7f567203b
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2025-12-23 12:56:47 +0100

    2025: Day 5.2

--- 2025/05/solution.swift
+++ 2025/05/solution.swift
@@ -1,32 +1,50 @@
-var bounds: [(Int, Int)] = []
+struct Bound: Hashable {
+    let l: Int
+    let r: Int
+}
+
+var bounds = Set<Bound>()
+
+func insert(b: Bound) {
+    // print("insert \(b) in \(bounds)")
+
+    for bound in bounds {
+        let l = bound.l
+        let r = bound.r
+
+        if l <= b.l && b.l <= r && l <= b.r && b.r <= r {
+            // print("\t already contained in \(bound), do nothing")
+            return
+        } else if b.l <= l && l <= b.r && b.l <= r && r <= b.r {
+            // print("\t contains existing \(bound), 'subsume' bound")
+            bounds.remove(bound)
+            return insert(b: b)
+        } else if b.l < l && l <= b.r && b.r <= r {
+            // print("\t overlap-left: left outside, right within \(bound)")
+            bounds.remove(bound)
+            let new = Bound(l: b.l, r: r)
+            return insert(b: new)
+        } else if l <= b.l && b.l <= r && r < b.r {
+            // print("\t overlap-right: left within \(bound), right outside")
+            bounds.remove(bound)
+            let new = Bound(l: l, r: b.r)
+            return insert(b: new)
+        }
+    }
+
+    // print("\t no overlap")
+    bounds.insert(b)
+}
+
 while let line = readLine() {
     let x = line.split(separator: "-").map { Int($0)! }
     if x.isEmpty { break }
 
-    bounds.append((x[0], x[1]))
+    insert(b: Bound(l: x[0], r: x[1]))
 }
 
-bounds.sort(by: {
-    if $0.1 == $1.1 {
-        return $0.0 < $1.0
-    }
-    return $0.1 < $1.1
-})
-
-
 var total = 0
-while let line = readLine() {
-    let num = Int(line)!
-    
-    for (l, r) in bounds {
-        // If larger than upper bound, then proceed to a higher upper bound.
-        if num > r { continue }
-        // If smaller than lower bound, then no point in proceeding.
-        if num < l { continue } // why not break here????
-        
-        // If we reached here, l <= num <= r
-        total += 1
-        break;
-    }
+for b in bounds {
+    total += b.r - b.l + 1
 }
 print(total)
